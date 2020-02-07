b4d460180448ed6caf471f64ca682e02f330245e
JorWo <42422209+JorWo@users.noreply.github.com>
2020-02-07 09:24:23 -1000

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -7,11 +7,12 @@ $dbname = "school";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+/* Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully"."<br>";
+*/
 
 $sql = "SELECT * FROM students";
 $result = $conn->query($sql);
