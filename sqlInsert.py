982e8d6808315347e2f176fd416e6f78ca1a0ca3
JorWo <42422209+JorWo@users.noreply.github.com>
2020-02-07 08:20:39 -1000

    Update sqlInsert.py

--- sqlInsert.py
+++ sqlInsert.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="jorwon21", passwd="wong", db="jorwon21")
+db = MySQLdb.connect(host="localhost", user="user", passwd="password", db="school")
 #Create cursor
 
 cur = db.cursor(MySQLdb.cursors.DictCursor)
